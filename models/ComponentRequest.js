d9f1695bd88fcc31e54acd664f83028eae4e2906
tharu-33 <tharushichamodya03@gmail.com>
2024-10-22 12:28:36 +0530

    new commit1

---
+++ models/ComponentRequest.js
@@ -0,0 +1,17 @@
+// models/ComponentRequest.js
+
+const mongoose = require('mongoose');
+
+const componentRequestSchema = new mongoose.Schema({
+  partNo: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  dateOfNeed: { type: Date, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  status: {
+    type: String,
+    enum: ['Not Issued', 'Issued'],
+    default: 'Not Issued',
+  },
+});
+
+module.exports = mongoose.model('ComponentRequest', componentRequestSchema);
