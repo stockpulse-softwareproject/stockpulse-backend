2296a86ea3f33ffffa7af37115f7c509ad14c9ab
githmi99 <amarathuga@gmail.com>
2024-10-22 18:13:55 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://tharushichamodya03:0GjO7C9UkCWmuSlB@cluster0.iydktcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+const dbURI = process.env.NODE_ENV === 'test' ? 'mongodb://localhost:27017/test'  : 'mongodb+srv://tharushi:Tharushi123@stock0.amvvjur.mongodb.net/?retryWrites=true&w=majority&appName=Stock0'; // Original DB URI
+mongoose.connect(
+  dbURI,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log('Connected to the database');
+  console.log('Connected to the database');
 });
 
 module.exports = db;
